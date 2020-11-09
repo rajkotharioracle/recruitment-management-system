77affc07e4a82c7da835b4e777ae8a9266920335
Raj Kothari <raj.kothari@oracle.com>
2020-11-09 18:10:02 +0530

    message1

---
+++ ViewController/public_html/LoginBean1.java
@@ -0,0 +1,384 @@
+package view.bean;
+
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.view.rich.component.rich.input.RichInputDate;
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import javax.sql.DataSource;
+
+import javax.naming.InitialContext;
+
+import javax.servlet.http.HttpSession;
+
+//import view.common.MyADFUtil;
+import view.common.SessionUtils;
+import view.common.LoginDAO;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+import javax.swing.JOptionPane;
+import java.io.Serializable;
+
+import java.sql.SQLException;
+
+import javax.faces.bean.SessionScoped;
+
+import javax.swing.JFrame;
+
+import oracle.adf.view.rich.component.rich.RichPopup;
+
+
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCIteratorBinding;
+
+import oracle.binding.BindingContainer;
+import view.common.ADFUtils;
+import view.common.JSFUtils;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.binding.OperationBinding;
+
+import view.common.DataConnect;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean implements Serializable{
+    
+    private static final long serialVersionUID = 1094801825228386363L;
+    
+    private RichInputText usernameInputText;
+    private RichInputText passwordInputText;
+    
+    JFrame f;
+    private RichPopup myPopup;
+    private RichInputText candName;
+    private RichInputText candEmail;
+    private RichInputText candMobile;
+    private RichInputText candGender;
+    private RichInputDate candDob;
+    private RichInputText candPreEventId;
+    private RichInputText candId;
+
+    /*private String msg;
+
+    public String getMsg() {
+                    return msg;
+            }
+
+    public void setMsg(String msg) {
+                    this.msg = msg;
+            }*/
+
+    public LoginBean() {
+    }
+
+    public void setUsernameInputText(RichInputText usernameInputText) {
+        this.usernameInputText = usernameInputText;
+    }
+
+    public RichInputText getUsernameInputText() {
+        return usernameInputText;
+    }
+
+    public void setPasswordInputText(RichInputText passwordInputText) {
+        this.passwordInputText = passwordInputText;
+    }
+
+    public RichInputText getPasswordInputText() {
+        return passwordInputText;
+    }
+
+/*
+    public String loginAction() {
+        String username = usernameInputText.getValue().toString();
+        String password = passwordInputText.getValue().toString();
+
+        System.out.println("username: " + username + " - password: " + password);
+
+        PreparedStatement stat = null;
+        ResultSet rs = null;
+        Connection conn = null;
+
+
+        try
+        {
+            String sql = "Select email_id, password from AKMR_USERS_MASTER where email_id = '" + username + "' and password = '" + password + "'";
+            conn = MyADFUtil.getConnection();
+            stat = conn.prepareStatement(sql);
+            rs = stat.executeQuery();
+            while(rs.next())
+            {
+                HttpSession session = SessionUtils.getSession();
+                session.setAttribute("username", username);
+                //return "Dashboard";
+                //return "null";
+                return "1";
+            }
+        }
+        catch (Exception e)
+        {
+           e.printStackTrace();
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Incorrect Username and Passowrd", "Please enter correct username and Password"));
+        return "login";
+    }
+*/
+    
+   /* public String loginAction() {
+        String user = usernameInputText.getValue().toString();
+        String pwd = passwordInputText.getValue().toString();
+        
+            String valid = LoginDAO.validate(user, pwd);
+            if (valid!=null) {
+                    System.out.println("Valiiiiiid");
+                    HttpSession session = SessionUtils.getSession();
+                    session.setAttribute("username", user);
+                    System.out.println("SUCCESS "+ valid);
+                    switch (valid){
+                    case "admin" : return "Dashboard";
+                    case "interviewer" : return "interviewer_dashboard";
+                    case "candidate" : return "candidate_dashboard";
+                    default : return "login";
+                    }
+                    //return "Dashboard";
+            } else {
+                System.out.println("INValiiiiiid");
+                    //JOptionPane.showMessageDialog(null, "YOUR INFORMATION HERE", "InfoBox: " + "TITLE BAR MESSAGE", JOptionPane.INFORMATION_MESSAGE);
+                    //f=new JFrame();  
+                    //JOptionPane.showMessageDialog(f,"Hello, Welcome to Javatpoint.");
+                    
+                    RichPopup.PopupHints hints = new RichPopup.PopupHints();
+                    this.myPopup.show(hints);
+                    System.out.println("INValiiiiiid 3");
+        
+
+                return "login";
+            }
+    }*/
+
+   public String loginAction() {
+        String user = usernameInputText.getValue().toString();
+        System.out.println("User   "+ user);
+        String pwd = passwordInputText.getValue().toString();
+            String valid = LoginDAO.validate(user, pwd);
+            if (valid!=null) {
+                    System.out.println("Valiiiiiid");
+                    HttpSession session = SessionUtils.getSession();
+                    session.setAttribute("username", user);
+                    System.out.println("SUCCESS "+ valid);
+                    //Session management code
+                    DCBindingContainer bindings = (DCBindingContainer)ADFUtils.evaluateEL("#{bindings}");
+                    System.out.println("bindings....");
+                        OperationBinding opBinding = bindings.getOperationBinding("getUserId");
+                    System.out.println("opbindings....");
+                    String userID = (String)opBinding.execute();
+                    System.out.println("userID...." + userID);
+                    JSFUtils.setManagedBeanValue("sessionScope.loggedInUserID", ""+userID);
+                    //JSFUtils.setManagedBeanValue("sessionScope.loggedInUserID", ""+user);                    
+                    switch (valid){
+                    //case "admin" : return "adminHome.jsf";
+                    case "admin" : return "events_home.jsf";
+                    //case "interviewer" : return "interviewer_dashboard.jsf";
+                    case "interviewer" : return "scheduledInterviews.jsf";
+                    case "candidate" : return "candidate_dashboard.jsf";
+                    default : return "login.jsf";
+                    }
+                    //return "Dashboard";
+            } else {
+                System.out.println("INValiiiiiid");
+                    RichPopup.PopupHints hints = new RichPopup.PopupHints();
+                    this.myPopup.show(hints);
+                    System.out.println("INValiiiiiid 3");
+                return "login";
+            }
+    }
+
+
+    public void setMyPopup(RichPopup myPopup) {
+        this.myPopup = myPopup;
+        
+    }
+
+    public RichPopup getMyPopup() {
+        return myPopup;
+    }
+
+
+    public void setCandName(RichInputText candName) {
+        this.candName = candName;
+    }
+
+    public RichInputText getCandName() {
+        return candName;
+    }
+
+    public void setCandEmail(RichInputText candEmail) {
+        this.candEmail = candEmail;
+    }
+
+    public RichInputText getCandEmail() {
+        return candEmail;
+    }
+
+    public void setCandMobile(RichInputText candMobile) {
+        this.candMobile = candMobile;
+    }
+
+    public RichInputText getCandMobile() {
+        return candMobile;
+    }
+
+    public void setCandGender(RichInputText candGender) {
+        this.candGender = candGender;
+    }
+
+    public RichInputText getCandGender() {
+        return candGender;
+    }
+
+    public void setCandDob(RichInputDate candDob) {
+        this.candDob = candDob;
+    }
+
+    public RichInputDate getCandDob() {
+        return candDob;
+    }
+
+    public void setCandPreEventId(RichInputText candPreEventId) {
+        this.candPreEventId = candPreEventId;
+    }
+
+    public RichInputText getCandPreEventId() {
+        return candPreEventId;
+    }
+
+    public void setCandId(RichInputText candId) {
+        this.candId = candId;
+    }
+
+    public RichInputText getCandId() {
+        return candId;
+    }
+
+    public void addNewCandidate(ActionEvent actionEvent) throws SQLException {
+        // Add event code here...
+        System.out.println("Funtion called");
+               String name = candName.getValue().toString();
+               String email = candEmail.getValue().toString();
+               String mobile = candMobile.getValue().toString();
+               //int mobile=Integer.parseInt(candMobile.getValue().toString());
+               String gender = candGender.getValue().toString();
+               String dob = candDob.getValue().toString();
+               int preEventId=Integer.parseInt(candPreEventId.getValue().toString());
+               int id=Integer.parseInt(candId.getValue().toString());
+               Connection con = null;
+               PreparedStatement ps = null;
+               con = DataConnect.getConnection();
+               //ps = con.prepareStatement("Select EMAIL_ID, PASSWORD from AKMR_USERS_MASTER where EMAIL_ID = ? and PASSWORD = ?");
+               System.out.println("preparing stmt ....");
+               ps = con.prepareStatement("insert into akmr_users_master values(?,'candidate','#00000#',0)");
+               ps.setString(1, email);
+               System.out.println("executing stmt .... email "+email);
+               ps.executeUpdate();
+               System.out.println("First insert done");
+               //DataConnect.close(con);
+               //con = null;
+               //ps = null;
+               //con = DataConnect.getConnection();
+               ps = con.prepareStatement("insert into akmr_candidate_data values(?, ?, ?, ?, ?, ?, ?, NULL)");
+               ps.setString(1, name);
+               ps.setString(2, email);
+               ps.setString(3, mobile);
+               ps.setString(4, gender);
+               ps.setDate(5, java.sql.Date.valueOf("2013-09-04"));  
+               ps.setInt(6, preEventId);
+               ps.setInt(7, id);
+               int rs = ps.executeUpdate();
+               if(rs ==1){
+                   System.out.println("New candidate added");
+                   }
+               else{
+                   }
+    }
+}
+
+/*
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import view.common.LoginDAO;
+import view.common.SessionUtils;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
+//import com.journaldev.jsf.dao.LoginDAO;
+//import com.journaldev.jsf.util.SessionUtils;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean implements Serializable {
+
+	private static final long serialVersionUID = 1094801825228386363L;
+	
+	private String pwd;
+	private String msg;
+	private String user;
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	//validate login
+	public String validateUsernamePassword() {
+		boolean valid = LoginDAO.validate(user, pwd);
+		if (valid) {
+			HttpSession session = SessionUtils.getSession();
+			session.setAttribute("username", user);
+			return "admin";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_WARN,
+							"Incorrect Username and Passowrd",
+							"Please enter correct username and Password"));
+			return "login";
+		}
+	}
+
+	//logout event, invalidate session
+	public String logout() {
+		HttpSession session = SessionUtils.getSession();
+		session.invalidate();
+		return "login";
+	}
+}
+*/
